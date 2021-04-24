d00976d119f4e97b3d076afdd06d15647960e1d8
Rodrigo-Segui <rodrigo_segui@hotmail.com>
2021-04-24 01:53:53 -0300

    adicionando filhos

--- index.js
+++ index.js
@@ -4,35 +4,55 @@ let vacas = [{id: 1, valor: 6}, {id: 2, valor: 2}, {id: 3, valor: 7}, {id: 4, va
 let toros = [{id: 1, valor: 10}];
 let restricao = 2;
 let custo = 0;
+
 //Criando arvore
 var TreeModel = require('tree-model'),
     tree = new TreeModel(),
-    root = tree.parse({name: '1', children: [
-        vacas[0], 
-        vacas[1],
-        vacas[2],
-        vacas[3],
-        vacas[4],
+    
+    vaca1 = tree.parse(vacas[0]);
+    vaca2 = tree.parse(vacas[1]);
+    vaca3 = tree.parse(vacas[2]);
+    vaca4 = tree.parse(vacas[3]);
+    vaca5 = tree.parse(vacas[4]);
+
+    root = tree.parse({id: '1', children: [
+        vaca1, 
+        vaca2,
+        vaca3,
+        vaca4,
+        vaca5,
     ]});
 
+console.log(root);
+
 var tree = new TreeModel();
-
 let upper = 0;
-
 let upperArray = [];
 
+let vacasArray = [];
+
+for(let i = 0; i < vacas.length; i++) {
+    vacasArray.push(tree.parse(vacas[i]));
+}
+
+console.log('array de vacas');
+console.log(vacasArray);
 
 //calculando os indices
 [root].map((item) => {
-    let vaca = item.model.children;
+    let vaca = item.children;
     vaca.filter(obj => {
-        let media = (parseFloat(toros[0].valor) + parseFloat(obj.valor))/2;
-        upperArray.push({id: obj.id, media: media});
+        console.log(obj.model.model);
+        let media = (parseFloat(toros[0].valor) + parseFloat(obj.model.model.valor))/2;
+        upperArray.push({id: obj.model.model.id, media: media});
         
     })
 });
 
+
+
 // resultados upper bound 
+console.log('\nUpperArray:')
 console.log(upperArray);
 
 // tem que exluir vaca 2 e 4
@@ -51,15 +71,51 @@ for(let i = 0; i < upperArray.length; i++) {
          arrayRemover.push(vacaId);
     }
 }
-console.log(restricao);
-console.log(arrayRemover);
-console.log('upper: ' + upper);
+
+//console.log(restricao);
+//console.log(arrayRemover);
+//console.log('upper: ' + upper);
 console.log('array remover: ' + arrayRemover);
 
+//console.log(tree);
 
-//Fazer lógica para removelos da arvore
+//Remover da arvora
+root.all().forEach(function (node) {
+    vaca = node.model.model;
+    [vaca].filter(obj => {
+        if(obj != undefined) {
+            if (arrayRemover.indexOf(obj.id) != -1) {
+                node.drop();
+            }
+        }
+    })
+});
+
+//Adiciona filhos
+root.all().forEach(function (node) {
+    vaca = node.model.model;
+    [vaca].filter(obj => {
+        if(obj != undefined) {
+            for(let i = 0; i < vacasArray.length; i++) {
+                if(i+1 !== obj.id){
+                    let newNode = vacasArray[i];
+                    node.addChild(newNode);
+                }
+            }
+        }
+    })
+});
+
+console.log('1 nodo:')
+console.log(root.children[0].model);
+
+console.log('2 nodo:')
+console.log(root.children[1].model);
+
+console.log('3 nodo:')
+console.log(root.children[2].model);
 
 
 
-
-//** PENSAR EM ALGUMA REPETIÇÃO FOR, WHILE, ETC PRA QUE ISSO CONTEMPLE TD ARVORE*/
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+//CHUPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
