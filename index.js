7d2e29a938a8f18a3ce5db81392cde5ef7813b48
arthurteixeira <arthurtjardim@gmail.com>
2021-05-15 14:09:12 -0300

    15-05

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 //Tabelas sendo representadas como array de objetos
 
 let vacas = [{id: 1, valor: 6}, {id: 2, valor: 2}, {id: 3, valor: 7}, {id: 4, valor: 6}, {id: 5, valor: 8}, {id: 6, valor: 10}, {id: 7, valor: 33}];
-let toros = [{id: 1, valor: 10}];
+let toros = [{id: 1, valor: 10}, {id: 2, valor: 8}];
 
+toros.sort((a,b) => {
+    if (a.valor < b.valor)
+       return -1;
+    if (a.valor > b.valor)
+      return 1;
+    return 0;
+});
 
-let restricao = 2;
-let custo = 0;
+toros.reverse();
 
+let vacasExcluidas = [];
+
+for(let z=0; z < toros.length; z++){
 //Criando arvore
 var TreeModel = require('tree-model'),
     tree = new TreeModel(),
     
 filhos = [];
 
-console.log('vacas length: ' + vacas.length);
+//console.log('vacas length: ' + vacas.length);
 
 for(let i = 0; i < vacas.length; i++){
     filhos.push(tree.parse(vacas[i]));
@@ -21,7 +30,7 @@ for(let i = 0; i < vacas.length; i++){
 
 root = tree.parse({id: '1', children: filhos});
 
-console.log(root);
+//console.log(root);
 
 var tree = new TreeModel();
 let upper = 0;
@@ -33,15 +42,15 @@ for(let i = 0; i < vacas.length; i++) {
     vacasArray.push(tree.parse(vacas[i]));
 }
 
-console.log('array de vacas');
-console.log(vacasArray);
+//console.log('array de vacas');
+//console.log(vacasArray);
 
 //calculando os indices
 [root].map((item) => {
     let vaca = item.children;
     vaca.filter(obj => {
-        console.log(obj.model.model);
-        let media = (parseFloat(toros[0].valor) + parseFloat(obj.model.model.valor))/2;
+        //console.log(obj.model.model);
+        let media = (parseFloat(toros[z].valor) + parseFloat(obj.model.model.valor))/2;
         upperArray.push({id: obj.model.model.id, media: media});
         
     })
@@ -50,28 +59,39 @@ console.log(vacasArray);
 
 
 // resultados upper bound 
-console.log('\nUpperArray:')
-console.log(upperArray);
+//console.log('\nUpperArray:')
+//console.log(upperArray);
 
 // tem que exluir vaca 2 e 4
   
 let arrayRemover = [];
 
 /////////////////////////////////////////////////////////////
-
+let flagFor = 0;
 //Primeira rodada, vendo quais caminhos devem ser excluidos
 for(let i = 0; i < upperArray.length; i++) {
     vacaValor = Object.values(upperArray[i])[1];
     vacaId = Object.values(upperArray[i])[0]
 
-    if (vacaValor > upper) {
-        upper = vacaValor;
+    for(let j = 0; j < vacasExcluidas.length; j++){
+        if (vacaId == vacasExcluidas[j]){
+            arrayRemover.push(vacaId);
+        }
+        //flagFor = true;
     }
-    else {
-         arrayRemover.push(vacaId);
-    }
+
+    //if(flagFor == false){
+        if (vacaValor > upper) {
+            upper = vacaValor;
+        }
+        else {
+            arrayRemover.push(vacaId);
+        }
+    //}
+
+    //flagFor = 0;
 }
-console.log('array remover: ' + arrayRemover);
+//console.log('array remover: ' + arrayRemover);
 
 //Remover da arvora
 root.all().forEach(function (node) {
@@ -91,20 +111,29 @@ root.all().forEach(function (node) {
     [vaca].filter(obj => {
         if(obj != undefined) {
             for(let i = 0; i < vacasArray.length; i++) {
-                if(i+1 !== obj.id){
-                    let newNode = vacasArray[i];
-                    node.addChild(newNode);
+               //console.log('i: ' + i);
+                //console.log('objid ' + obj.id)
+                //console.log('\nArray Remover: ' + arrayRemover[i]);
+                //console.log('Array vacas: ' + vacasArray[i].model.id);
+                if (vacasArray[i].model.id != arrayRemover[i]){
+                    if(i+1 !== obj.id){
+                        if((arrayRemover.includes(vacasArray[i].model.id)) == false){
+                            let newNode = vacasArray[i];
+                            node.addChild(newNode);
+                            //console.log('entrou');
+                        }
+                    }
                 }
-            }
+           }
         }
     })
 });
 
-console.log(upperArray)
+//console.log(upperArray)
 
 let maiorArray = [];
 
-//ESSA FUNÇÃO
+
 for (let i = 0; i < root.children.length; i++) {
     let tamanho = root.children[i].model.children.length;
     let maior = {id: null, valor: 0};
@@ -150,8 +179,16 @@ console.log('Custos \n' + arrayCustos)
 
 let indice = indexOfMax(arrayCustos);
 
-console.log('Touro: 1 | Vacas: ' + arrayIds[indice] + ' e ' + segundaIds[indice]);
+let zindice = z+1;
 
+console.log('Touro: ' + zindice + ' | Vacas: ' + arrayIds[indice] + ' e ' + segundaIds[indice] + '\n\n');
+
+vacasExcluidas.push(arrayIds[indice]);
+vacasExcluidas.push(segundaIds[indice]);
+
+//console.log('vacas excluidas ' + vacasExcluidas);
+
+}
 function indexOfMax(arr) {
     if (arr.length === 0) {
         return -1;
